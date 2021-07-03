e2993b3abaf25356ebb8d84c4bd1617b9e38b74f
Sarthak <61661622+sg60692@users.noreply.github.com>
2021-07-03 17:41:14 +0530

    Our team

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header.js';
 import Service from './components/Services/Service.js';
 import Banner from './components/Banner/Banner.js';
 import Product from './components/Products/Products.js';
+import Ourteam from './components/Ourteam/Ourteam';
 import Testimonials from './components/Testimonials/Testimonials.js';
 import Contact from './components/Contact/Contact.js';
 import Footer from './components/Footer/Footer.js';
@@ -14,7 +15,7 @@ function App() {
       <Banner/>
       <Service/>      
       <Product/>
-      <Testimonials/>
+      <Ourteam/>
       <Contact/>
       <Footer/>
     </div>
