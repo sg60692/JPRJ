8690c6ae3f2cda3e225ef169e7342290262ce36a
Sarthak <61661622+sg60692@users.noreply.github.com>
2021-10-14 23:53:40 +0530

    fixing

--- client/src/components/Services/Service.js
+++ client/src/components/Services/Service.js
@@ -18,10 +18,20 @@ function Service() {
             </h4>
           </div>
         </div>
+                
         <div class="wrapper">
             <div class="text-center f ">                
                 <div class="d-flex flex-wrap container">
-                <AnimationRightLeft direction="left"className="comp-8 animate-left service-large">
+                    <AnimationRightLeft direction="left"className="comp-8 animate-left service-large">
+                            <div>
+                                <div class="icon-Manufacturing icon"/>
+                            </div> 
+                            <p class="sub-caption">
+                                Leverage competitive demand using faster and 
+                                more efficient manufacturing.                                       
+                            </p>
+                    </AnimationRightLeft>
+                    <AnimationZoom direction="left"className="comp-8 animate-left service-large">
                         <div>
                             <div class="icon-Manufacturing icon"/>
                         </div> 
@@ -29,8 +39,8 @@ function Service() {
                             Leverage competitive demand using faster and 
                             more efficient manufacturing.                                       
                         </p>
-                </AnimationRightLeft>
-                <AnimationZoom direction="left"className="comp-8 animate-left service-large">
+                    </AnimationZoom>
+                    <AnimationRightLeft direction="right"className="comp-8 animate-left service-large">
                         <div>
                             <div class="icon-Manufacturing icon"/>
                         </div> 
@@ -38,19 +48,31 @@ function Service() {
                             Leverage competitive demand using faster and 
                             more efficient manufacturing.                                       
                         </p>
-                </AnimationZoom>
-                <AnimationRightLeft direction="right"className="comp-8 animate-left service-large">
-                        <div>
-                            <div class="icon-Manufacturing icon"/>
-                        </div> 
-                        <p class="sub-caption">
-                            Leverage competitive demand using faster and 
-                            more efficient manufacturing.                                       
-                        </p>
-                </AnimationRightLeft>                   
+                    </AnimationRightLeft>                   
                 </div>
             </div>
-            
+            <div class="text-center fc">
+                <div class="d-flex flex-wrap container">
+                    <AnimationZoom direction="left"className="comp-8 animate-left ml-auto">
+                        <div>
+                            <div class="icon-Manufacturing icon"/>
+                        </div> 
+                        <p class="sub-caption">
+                            Leverage competitive demand using faster and 
+                            more efficient manufacturing.                                       
+                        </p>
+                    </AnimationZoom>                       
+                    <AnimationZoom direction="left"className="comp-8 animate-left mr-auto">
+                        <div>
+                            <div class="icon-Manufacturing icon"/>
+                        </div> 
+                        <p class="sub-caption">
+                            Leverage competitive demand using faster and 
+                            more efficient manufacturing.                                       
+                        </p>
+                    </AnimationZoom> 
+                </div>
+            </div>
         </div>
       </section>
     </div>
