0d652d34e448c777a62214478b67c07c3f0f727c
Sarthak <61661622+sg60692@users.noreply.github.com>
2021-10-14 23:58:39 +0530

    trying to fix product

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,18 +1,15 @@
 import React from 'react'
-import { useRef ,useEffect ,useState} from 'react';
 import Project_1 from "../../images/Project_1.webp";
 import './Products.scoped.css';
-
 function Products() {
     return (
         <div id="Product">
             <section class="fold fold-4 bg-dark">
-                <div class="container" >                    
-                    <h2 class="text-center" className="ok1">One Tag, </h2>
-                    <h2 class="text-center" className="ok2"> Many Possibilities.</h2>
+                <div class="container">
+                    <h2 class="text-center">One Tag, Many Possibilities.</h2>
                     <div class="d-flex align-center justify-content-even comp-1 comp-7">
-                        <div class="img animate-left" >
-                            <img src={Project_1}  class="img-responsive" alt="Search Ads" />
+                        <div class="img animate-left">
+                            <img src={Project_1} class="img-responsive" alt="Search Ads"/>
                         </div>
                         <div class="content-wrap info-wrapper animate-right">
                             <h3 class="title">Services</h3>
