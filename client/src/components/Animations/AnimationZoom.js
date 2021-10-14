9da7b5091580217527fc1782d17a54781eb63bda
Sarthak <61661622+sg60692@users.noreply.github.com>
2021-10-14 17:36:17 +0530

    Boop-Zoom

--- client/src/components/Animations/AnimationZoom.js
+++ client/src/components/Animations/AnimationZoom.js
@@ -1,37 +1,41 @@
 import { useState, useEffect,useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-const Boop1 = ({ rotation = 20, timing = 200, children }) => {
-  const [isBooped, setIsBooped] = useState(false);
+import React from 'react'
+
+function AnimationZoom(children) {
+  const [isZoomed, setIsZoomed] = useState(false);
   const style = {
     display: "flex",
     backfaceVisibility: "hidden",
-    transform: isBooped
+    transform: isZoomed
       ? `rotate(0deg)
             scale(1.3)
             translate(-10px, 0px)
            `
       : `rotate(0deg)`,
-    transition: `transform ${timing}ms`,
+    transition: `transform 500ms`,
   };
   useEffect(() => {
-    if (!isBooped) {
+    if (!isZoomed) {
       return;
     }
     const timeoutId = window.setTimeout(() => {
-      setIsBooped(false);
-    }, timing + 200);
+      setIsZoomed(false);
+    },500);
     return () => {
       window.clearTimeout(timeoutId);
     };
-  }, [isBooped, timing]);
+  }, [isBooped]);
   const trigger = () => {
     setIsBooped(true);
   };
   
   return (
-    <span onMouseEnter={trigger} style={style}>
+    <span onMouseEnter={trigger}>
       {children}
     </span>
   );
-};
+}
+
+export default AnimationZoom
